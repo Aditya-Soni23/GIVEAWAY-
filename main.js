98eee3a16b0694dda63bb0df4b8b9f08916216cf
Aditya-Soni23 <77341618+Aditya-Soni23@users.noreply.github.com>
2021-07-22 08:42:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+function jij(){
+    window.location = "giveaway.html";
+}
+
+
+
+
+
+function xt(){
+    window.location = "process.html";
+}
+
+
+var freefireid = 0;
+var diamonds = 0;
+var gold = 0;
+
+
+
+
+
+
+
+function xxx(){
+
+
+
+
+
+    
+    freefireid = Number(document.getElementById("ffid").value);
+    gold = Number(document.getElementById("go").value);
+    diamonds = Number(document.getElementById("dia").value);
+
+
+
+
+document.getElementById("iii").innerHTML = freefireid;
+document.getElementById("ggg").innerHTML = gold;
+document.getElementById("ddd").innerHTML = diamonds;
+}
+
+
+
+
+
+function ppp(){
+    window.location = "thankyou.html";
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
